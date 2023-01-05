cc6e71c6c04f4a40a4b1ad87228c5c44afe44c1a
karamage <karamage.umauma@gmail.com>
2023-01-05 11:25:02 +0900

    add rollup

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <div class="text">ライブラリ</div>
+  <SampleText />
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import SampleText from './SampleText.vue'
 
 export default {
   name: 'App',
   components: {
-    // HelloWorld
+    SampleText
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-.text {
-  font-size: xx-large;
-  color: crimson;
-}
-</style>
---
+++ src/SampleText.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <div class="text">ライブラリ</div>
+</template>
+
+<script>
+export default {
+  name: 'SampleText',
+  components: {}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.text {
+  font-size: xx-large;
+  color: #ff00ff;
+}
+</style>
